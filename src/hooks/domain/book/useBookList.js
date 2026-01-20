57185fb3fd1d974706b31ea6ff997baf756bfd4d
jeongseokhyeon <jeong9235@gmail.com>
2026-01-20 15:37:11 +0900

    fix: 상세 검색 패널 내 초기화 기능 수정

--- src/hooks/domain/book/useBookList.js
+++ src/hooks/domain/book/useBookList.js
@@ -59,6 +59,9 @@ export function useBookList() {
             bookAuthor: '',
             publisherName: '',
             bookIsbn: '',
+            publicationDateStart: '',
+            publicationDateEnd: '',
+            category: '',
             sort: 'createdAt,desc'
         });
     };
