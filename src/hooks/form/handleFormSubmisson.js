720ed594978fb1f7fe673e1d7152d5bc8bf3f617
jeongseokhyeon <jeong9235@gmail.com>
2026-02-18 15:40:36 +0900

    chore:불필요한 콘솔 확인 코드 삭제

--- src/hooks/form/handleFormSubmisson.js
+++ src/hooks/form/handleFormSubmisson.js
@@ -13,13 +13,8 @@ export const handleFormSubmission = async ({
 
     setFieldErrors({});
 
-    console.log(form)
-    console.log(validateFunc)
-    console.log(apiFunc)
-
     // 클라이언트 측 검증
     const clientErrors = validateFunc(form);
-    console.log(clientErrors)
     if (Object.keys(clientErrors).length > 0) {
         setFieldErrors(clientErrors);
         toast.error("입력한 정보를 다시 확인해주세요.");
@@ -38,6 +33,6 @@ export const handleFormSubmission = async ({
             const newFieldErrors = parserServerValidationErrors(serverError);
             setFieldErrors(newFieldErrors);
         }
-        toast.error(serverError?.message || "입력한 정보를 다시 확인해주세요.");
+        toast.error(serverError?.data || "입력한 정보를 다시 확인해주세요.");
     }
 };
