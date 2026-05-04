4ff9ba0ce8c5cf58dbb267db103b2c99d6173f00
jeongseokhyeon <jeong9235@gmail.com>
2026-05-04 22:18:45 +0900

    fix:필드 에러 누락  수정

--- src/components/book/BookForm.jsx
+++ src/components/book/BookForm.jsx
@@ -37,24 +37,24 @@ const BookForm = ({
                             onChange={handlers.handleThumbnailChange}
                             style={{ display: 'none' }}
                         />
-                        <ErrorMsg message={errors.bookThumbnail} />
+                        <ErrorMsg message={errors.fieldErrors.bookThumbnail} />
                     </div>
                     <div style={{ flex: 1, display: 'flex', flexDirection: 'column', gap: '20px' }}>
                         <div>
                             <label className='input-label'>도서 제목</label>
                             <input name="bookTitle" value={formData.bookTitle} onChange={handlers.handleChange} className='input-style' placeholder="제목이 자동 입력됩니다" />
-                            <ErrorMsg message={errors.bookTitle} />
+                            <ErrorMsg message={errors.fieldErrors.bookTitle} />
                         </div>
                         <div style={{ display: 'flex', gap: '15px' }}>
                             <div style={{ flex: 1 }}>
                                 <label className='input-label'>카테고리</label>
                                 <input name="bookCategory" value={formData.bookCategory} onChange={handlers.handleChange} className='input-style' />
-                                <ErrorMsg message={errors.bookCategory} />
+                                <ErrorMsg message={errors.fieldErrors.bookCategory} />
                             </div>
                             <div style={{ flex: 1 }}>
                                 <label className='input-label'>ISBN</label>
                                 <input name="bookIsbn" value={formData.bookIsbn} onChange={handlers.handleChange} className='input-style' />
-                                <ErrorMsg message={errors.bookIsbn} />
+                                <ErrorMsg message={errors.fieldErrors.bookIsbn} />
                             </div>
                         </div>
                     </div>
@@ -64,29 +64,29 @@ const BookForm = ({
                     <div>
                         <label className='input-label'>저자</label>
                         <input name="bookAuthor" value={formData.bookAuthor} onChange={handlers.handleChange} className='input-style' />
-                        <ErrorMsg message={errors.bookAuthor} />
+                        <ErrorMsg message={errors.fieldErrors.bookAuthor} />
                     </div>
                     <div>
                         <label className='input-label'>역자</label>
                         <input name="bookTranslator" value={formData.bookTranslator} onChange={handlers.handleChange} className='input-style' />
-                        <ErrorMsg message={errors.bookTranslator} />
+                        <ErrorMsg message={errors.fieldErrors.bookTranslator} />
                     </div>
                     <div>
                         <label className='input-label'>출판사</label>
                         <input name="publisherName" value={formData.publisherName} onChange={handlers.handleChange} className='input-style' />
-                        <ErrorMsg message={errors.publisherName} />
+                        <ErrorMsg message={errors.fieldErrors.publisherName} />
                     </div>
                     <div>
                         <label className='input-label'>출판일</label>
                         <input type="date" name="publicationDate" value={formData.publicationDate} onChange={handlers.handleChange} className='input-style' />
-                        <ErrorMsg message={errors.publicationDate} />
+                        <ErrorMsg message={errors.fieldErrors.publicationDate} />
                     </div>
                 </div>
 
                 <div style={{ marginBottom: '20px' }}>
                     <label className='input-label input-unique-label'>청구기호 (필수)</label>
                     <input name="bookCallNumber" value={formData.bookCallNumber} onChange={handlers.handleChange} className='input-style input-unique-style' placeholder="예: 813.6-박24-ㄴ" />
-                    <ErrorMsg message={errors.bookCallNumber} />
+                    <ErrorMsg message={errors.fieldErrors.bookCallNumber} />
                 </div>
 
                 <div>
@@ -96,12 +96,14 @@ const BookForm = ({
                         onChange={handlers.handleEditorChange}
                         height="300px"
                     />
-                    <ErrorMsg message={errors.bookContents} />
+                    <ErrorMsg message={errors.fieldErrors.bookContents} />
                 </div>
 
                 <div className='form-btn-group'>
                     <button type="button" className="cnl-btn" onClick={handlers.handleCancel}>취소</button>
-                    <button type="submit" className='input-btn'>도서 등록 완료</button>
+                    <button type="submit" className="input-btn" disabled={loading}>
+                        {loading ? processingText : submitBtnText}
+                    </button>
                 </div>
             </form>
         </div>
