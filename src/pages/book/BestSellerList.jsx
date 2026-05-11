8591f495f45b2f7549e2fbab22c7ccc8c366bb16
jeongseokhyeon <jeong9235@gmail.com>
2026-05-12 05:36:45 +0900

    chore:사용되지 않는 파일 삭제

--- src/pages/book/BestSellerList.jsx
+++
@@ -1,8 +0,0 @@
-import BestSellerListView from "@/components/book/BestSellerListView";
-import { useBestSeller } from "@/hooks/domain/book/bestseller/useBestSeller";
-
-function BestSellerList() {
-    return <BestSellerListView {...useBestSeller()} />;
-}
-
-export default BestSellerList;
