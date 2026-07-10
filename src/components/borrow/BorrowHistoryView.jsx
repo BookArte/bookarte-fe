bacfb95f3f82e21c6094c80fcb60198fa3061b90
jeongseokhyeon <jeong9235@gmail.com>
2026-07-10 12:41:13 +0900

    fix: 화면 줄일 시 도서명과 다른 텍스트가 겹치는 현상 수정

--- src/components/borrow/BorrowHistoryView.jsx
+++ src/components/borrow/BorrowHistoryView.jsx
@@ -28,9 +28,9 @@ function BorrowHistoryView({ borrows, status, handlers }) {
             <td className="number-column">{index + 1 + currentPage * 5}</td>
             <td className="book-info-td">
                 <img src={item.bookThumbnail} alt="" className="mini-thumb" />
-                <div>
-                    <div className="book-title">{item.bookTitle}</div>
-                    <div className="book-author">{item.bookAuthor}</div>
+                <div className="book-status-text">
+                    <div className="book-title" title={item.bookTitle}>{item.bookTitle}</div>
+                    <div className="book-author" title={item.bookAuthor}>{item.bookAuthor}</div>
                 </div>
             </td>
             <td className="member-info-td">
